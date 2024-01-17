80c53c4e03539dc1119fdfbde2eeb4e56eed35f9
王杰 <jie985563349@outlook.com>
2024-01-17 14:35:04 +0800

    add install button

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
+
+const installed = ref(true);
+let installPrompt: any = null;
+
+onBeforeMount(() => {
+  window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installPrompt = event;
+    installed.value = false;
+    return false;
+  });
+
+  window.addEventListener('appinstalled', () => {
+    installed.value = true;
+  });
+});
+
+const install = async () => {
+  if (!installPrompt) {
+    return;
+  }
+  await installPrompt.prompt();
+  installPrompt = null;
+};
+</script>
+
 <template>
   <div>
     <router-view v-slot="{ Component }">
@@ -5,6 +33,14 @@
         <component :is="Component" />
       </keep-alive>
     </router-view>
+
+    <button
+      v-if="!installed"
+      class="fixed bottom-10 right-10 block px-4 py-2 rounded bg-purple-500 hover:bg-purple-700"
+      @click="install"
+    >
+      Install
+    </button>
   </div>
 </template>
 
